6f944980dd31924af849d590c5e8ea8f4dbc014c
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-08-26 12:48:59 -0300

    Update db_erp.sql

--- src/database/db_erp.sql
+++ src/database/db_erp.sql
@@ -1,3 +1,12 @@
-create database if not exists erp_ind;
+create database if not exists bd_erp;
 
-use erp_ind;
+create table if not exists usuarios(
+id_usuario int not null,
+nome varchar (100),
+endereco varchar(100),
+rg varchar(100),
+cpf bigint(13),
+
+primary key(id_usuario)
+
+);
