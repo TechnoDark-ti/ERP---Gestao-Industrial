6db53d1a6c2322bf053a6bdd01673ff6feb66d7d
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-06 21:55:46 -0300

    Update db_erp.sql

--- src/database/db_erp.sql
+++ src/database/db_erp.sql
@@ -1,3 +1,11 @@
 create database if not exists sistemaERP;
 
 use erp_ind;
+
+
+create table produtos(
+    id_produto int auto_increment,
+    nome_produto varchar (50),
+    preco_produto decimal (10,2)
+);
+
