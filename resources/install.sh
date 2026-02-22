30e2411e7c06a4104f1fdb847d1c29fd069acf8e
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2026-02-22 20:17:49 -0300

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -2,4 +2,10 @@
 
 sudo apt install gcc g++ default-mysql-server default-mysql-client
 
+sudo fallocate -l 4G /media/sd/swapfile
+sudo chmod 600 /media/sd/swapfile
+sudo mkswap /media/sd/swapfile
+sudo swapon /media/sd/swapfile
 
+
+sudo apt install nginx postgresql php8.2-fpm php8.2-mbstring php8.2-xml php8.2-pgsql php8.2-zip php8.2-curl php8.2-opcache composer nodejs npm
