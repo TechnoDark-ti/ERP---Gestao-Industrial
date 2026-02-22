dda60f0f961983f9695b3a96f105f00c66563f8d
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2026-02-22 20:20:39 -0300

    Update db_erp.sql

--- src/database/db_erp.sql
+++ src/database/db_erp.sql
@@ -9,3 +9,13 @@ create table produtos(
     preco_produto decimal (10,2)
 );
 
+
+create usuarios(
+    id user int not null,
+    nome varcahr (50),
+    cpf bigint (13),
+    rg bigint (13),
+    telefone int (13),
+
+
+)
